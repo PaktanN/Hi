df11f814bed7c94e3187adc6569450bdec8d3bee
PaktaN <kirocinka69@mail.ru>
2026-01-18 16:37:17 +0300

    0

--- 05_lesson/lesson05_task4.py
+++ 05_lesson/lesson05_task4.py
@@ -6,13 +6,13 @@ driver = webdriver.Firefox()
 try:
     driver.get("http://the-internet.herokuapp.com/login")
     
-    driver.find_element(By.ID, "username").send_keys("Dasha")
-    driver.find_element(By.ID, "password").send_keys("123456789")
-
+    driver.find_element(By.ID, "username").send_keys("tomsmith")
+    driver.find_element(By.ID, "password").send_keys("SuperSecretPassword!")
+    
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
     
-    flash_message = driver.find_element(By.ID, "flash").text
-    print(flash_message)
+    flash = driver.find_element(By.ID, "flash").text
+    print(flash)
     
 finally:
     driver.quit()
